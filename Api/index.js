6147f962eca2bc0737f75531f67640608d193ce2
Celes Flowers <seleccionfloresceleste@gmail.com>
2023-04-19 18:27:43 -0300

    RefactoringBackForDeploy

--- Api/index.js
+++ Api/index.js
@@ -3,14 +3,16 @@ const { conn } = require('./src/db.js');
 const {getAllGenres} = require('./src/controllers/ApyAndDb/getGenreData')
 const {insertGamesDb} = require('./src/controllers/ApyAndDb/insertGamesApiDB')
 const {getAllPlatforms} = require('./src/controllers/ApyAndDb/getPlatforms')
-const PORT = 3001;
+//const PORT = 3001;
+require ('dotenv').config();
+
 
 
 conn.sync({ alter: true }).then(() => {
   // getAllGenres()
   // getAllPlatforms()
   // insertGamesDb()
-  app.listen(3001, () => {
-  console.log('%s listening at 3001'); // eslint-disable-line no-console
+  app.listen(process.env.PORT, () => {
+  console.log('%s listening at',process.env.PORT); // eslint-disable-line no-console
   });
 }); 
--- Api/src/db.js
+++ Api/src/db.js
@@ -1,7 +1,7 @@
 const { Sequelize, Op } = require('sequelize');
 require('dotenv').config();
 const {
-   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = process.env;
 
 const modelVideoGames = require('./models/VideoGame.js')
@@ -12,7 +12,7 @@ const modelWallets = require('./models/Wallet');
  
 
 const db = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
    {
       logging: false,
    }
